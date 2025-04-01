3c9c2cefb8b7d7aed6f28db0bf19ec426d6221a9
Francis <brakay1136@gmail.com>
2025-04-01 10:41:34 -0700

    first chrome extension

---
+++ contentScript.js
@@ -0,0 +1,17 @@
+let hour =  document.querySelector('.hour')
+let munite =  document.querySelector('.minute')
+let second =  document.querySelector('.second')
+const play = document.querySelector('.fa-play')
+
+
+play.addEventListener('click', () => {
+
+    setInterval(() => {
+        if(second.value === "0"){
+            second.value ="59";
+            munite.value--
+        }
+        second.value--
+
+    }, 1000)
+})
