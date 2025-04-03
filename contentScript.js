04a730f7cac414103ba634a6143d4f4d053596f8
Francis <brakay1136@gmail.com>
2025-04-03 14:49:24 -0700

    debugging timer function

--- contentScript.js
+++ contentScript.js
@@ -12,33 +12,56 @@ function stopTimer(){
 
 
 //continue to debug timer function tomorrow
+//if hour > 0 and minutes === 0 and seconds === 0
+    //second.value = 59
+    //minute.value = 59
+    //minute.value - 1
+    //hour.value - 1
+//if hour === 0
+    //hour.value = 0 
+    //minute.value = 59
+    //second.value = 59
+    //minute.value - 1
+//if minute === 0
+    //minute.value = 0
+    //second.value = 59
+//if seconds === 0
+    // cancel timer or countDown
+
+
+
+
+
 function timer(){
    let timer =  setInterval(() => {
 
     let hours = Number(hour.value)
     let minutes = Number(minute.value)
     let seconds = Number(second.value)
+
         if(hours > 0 && minutes !== 0 && seconds === 0){
             second.value ="59";
             minute.value = String(minutes - 1)
         } 
+      
         else if(hours > 0 && minutes === 0 && seconds === 0){
-            second.value ="59";
+            // second.value ="59";
             minute.value = "59"
-            minute.value = String(minutes - 1);
             hour.value = String(hours - 1);
         } 
-        else if(hour === 0 ){
-            second.value ="59";
+        else if(hours === 0 ){
+            hour.value = String(hours = 0)
+            // second.value ="59";
             minute.value = "59";
 
         }
-        else if(minute === 0){
-            second.value = "59";
+        else if(minutes === 0){
+            minute.value = String(minutes = 0)
+            // second.value = "59";
         }
-        else if (hours === 0 && minutes === 0 && seconds === 0){
-            clearInterval(timer)
-        }
+        // else if (seconds === 0){
+        //     clearInterval(timer)
+        // }
         else{
             second.value = String(seconds - 1);
         }
@@ -59,3 +82,27 @@ play.addEventListener('click', timer)
 reset.addEventListener('click', resetTimer)
 
 
+// if(hours > 0 && minutes !== 0 && seconds === 0){
+//     second.value ="59";
+//     minute.value = String(minutes - 1)
+// } 
+// else if(hours > 0 && minutes === 0 && seconds === 0){
+//     second.value ="59";
+//     minute.value = "59"
+//     minute.value = String(minutes - 1);
+//     hour.value = String(hours - 1);
+// } 
+// else if(hour === 0 ){
+//     second.value ="59";
+//     minute.value = "59";
+
+// }
+// else if(minute === 0){
+//     second.value = "59";
+// }
+// else if (hours === 0 && minutes === 0 && seconds === 0){
+//     clearInterval(timer)
+// }
+// else{
+//     second.value = String(seconds - 1);
+// }
