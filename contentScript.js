ae97086e0ea25224019676f7d20b94a555170262
Francis <brakay1136@gmail.com>
2025-04-07 14:26:54 -0700

    improved functionaliy

--- contentScript.js
+++ contentScript.js
@@ -2,98 +2,60 @@ let hour =  document.querySelector('.hour')
 let minute =  document.querySelector('.minute')
 let second =  document.querySelector('.second')
 const play = document.querySelector('.fa-play')
+const pause = document.querySelector('.fa-pause')
 const reset = document.querySelector('.fa-rotate-right')
-
-
-
-function stopTimer(){
-    clearInterval(timer)
-}
-
-
-//continue to debug timer function tomorrow
-//if hour > 0 and minutes === 0 and seconds === 0
-    //second.value = 59
-    //minute.value = 59
-    //minute.value - 1
-    //hour.value - 1
-//if hour === 0
-    //hour.value = 0 
-    //minute.value = 59
-    //second.value = 59
-    //minute.value - 1
-//if minute === 0
-    //minute.value = 0
-    //second.value = 59
-//if seconds === 0
-    // cancel timer or countDown
-
-
-
     
-   
+let timer;
     
     function countDown(){
-       let hours = Number(hour.value) 
-       let minutes = Number(minute.value) 
-       let seconds = Number(second.value) 
-        let timer =  setInterval(() => {
-            if(seconds < 0 ){
-                seconds = 59;
+        let hours = hour.value
+        let minutes = minute.value 
+        let seconds = second.value 
+        timer =  setInterval(() => {
+            if(hours > 0 && minutes == 0 && seconds == 0 ){
+                seconds = 60;
                 minutes = 59;
+                hours--
+                hour.value = hours;
             } 
-
-            if (seconds === 0){
-                 minutes = Number(minute.value) - 1;
-            }
-
-           
             
-           
-
-            minute.value = String(minutes)
-            second.value = String(seconds--)
+            if (hours == 0 && minutes > 0 && seconds == 0){
+                seconds = 60;
+                minutes--
+                minute.value = minutes
+            }
+            
+            if (hours == 0 && minutes == 0 && seconds == 0){
+                clearInterval(timer);
+                return
+            }
+            
+            minute.value = minutes
+            seconds--
+            second.value = seconds;
         }, 1000)
+        
+    }
     
     
+    function resetTimer(){
+        hour.value = '0'
+        minute.value = '0'
+        second.value = '0'
+        clearInterval(timer)
+        return
+    }
 
-}
-
-
-function resetTimer(){
-    hour.value = '0'
-    munite.value = '0'
-    second.value = '0'
-    clearInterval(timer)
-}
-
-
-
-play.addEventListener('click', countDown)
-reset.addEventListener('click', resetTimer)
+    function pauseTimer(){
+        clearInterval(timer)
+    }
 
 
-// if(hours > 0 && minutes !== 0 && seconds === 0){
-//     second.value ="59";
-//     minute.value = String(minutes - 1)
-// } 
-// else if(hours > 0 && minutes === 0 && seconds === 0){
-//     second.value ="59";
-//     minute.value = "59"
-//     minute.value = String(minutes - 1);
-//     hour.value = String(hours - 1);
-// } 
-// else if(hour === 0 ){
-//     second.value ="59";
-//     minute.value = "59";
 
-// }
-// else if(minute === 0){
-//     second.value = "59";
-// }
-// else if (hours === 0 && minutes === 0 && seconds === 0){
-//     clearInterval(timer)
-// }
-// else{
-//     second.value = String(seconds - 1);
-// }
+play.addEventListener('click', countDown);
+reset.addEventListener('click', resetTimer);
+pause.addEventListener('click', pauseTimer);
+
+
+
+
